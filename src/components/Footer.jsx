985143bd30220dac923a4d6c0e06636adcde20b6
ceydamuslu <cydamslu53@gmail.com>
2024-11-06 15:21:48 +0300

    data eklentisi

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
 export default function Footer() {
     return(
         <>
+        <div className="w-[1440px] h-[454px] bg-white">
+            <div className="w-[483px] h-76 pl-[479px]">
+                <h2 className="w-[483px] h-[72px] font-bold text-5xl text-violet-700">
+
+                </h2>
+            </div>
+        </div>
         </>
     )
 } 
---
+++ src/data/data.js
@@ -0,0 +1,77 @@
+
+
+
+export const data= {
+    
+    header: {
+        name: "Ceyda",
+    }, 
+    
+    hero: {
+        role: 'I am a Frontend Developer...',
+        description:
+        "...who likes to craft solid and scalable frontend products with great user experiences.",
+    },
+
+    skills: {
+        title: 'Yetenekler',
+            javascript:"JAVASCRIPT",
+            react:"REACT",
+            redux:"REDUX",
+            node:"NODE",
+            vscode:"VS CODE",
+            figma:"FIGMA",
+    },
+
+    profile: {
+        title: 'Profil',
+            details: [
+              {
+                label: 'Doğum Tarihi',
+                value: '26.04.2001',
+              },
+              {
+                label: 'İkamet Şehri',
+                value: 'Trabzon',
+              },
+              {
+                label: 'Eğitim Durumu',
+                value: 'Karadeniz Teknik Üniversitesi Türk Dili ve Edebiyatı Lisans, 2024',
+              },
+              {
+                label: 'Tercih Ettiği Rol',
+                value: 'Frontend, UI',
+              },
+            ],
+
+            aboutMe: {
+                title: 'Hakkımda',
+                description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.
+                Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! 
+                `,
+              },
+            }, 
+         
+    projects: [
+        {
+          title: 'Workintech',
+          description:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          icons: ['React', 'Redux', 'Vercel'],
+          
+        },
+        {
+          title: 'Journey',
+          description:
+            'A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.',
+          icons: ['React', 'Redux', 'Vercel'],
+          
+        },
+    ],
+    
+    footer: {
+        title: "Send me a message !",
+        description: "Got a question or proposal, or just want to say hello ? Go ahead."
+    }
+    }
+
