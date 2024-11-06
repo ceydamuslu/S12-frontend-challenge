4d4ad60596f18c5a381113c6dac6b93eb5071e55
ceydamuslu <cydamslu53@gmail.com>
2024-11-06 17:01:10 +0300

    data yeni

--- src/data/data.js
+++ src/data/data.js
@@ -1,9 +1,10 @@
 
 
 
-export const data= {
-    
-    header: {
+const data = {
+
+    en: {
+      header: {
         name: "Ceyda",
     }, 
     
@@ -11,6 +12,78 @@ export const data= {
         role: 'I am a Frontend Developer...',
         description:
         "...who likes to craft solid and scalable frontend products with great user experiences.",
+    },
+
+    skills: {
+        title: 'Skills',
+            javascript:"JAVASCRIPT",
+            react:"REACT",
+            redux:"REDUX",
+            node:"NODE",
+            vscode:"VS CODE",
+            figma:"FIGMA",
+    },
+
+    profile: {
+        title: 'Profile',
+            details: [
+              {
+                label: 'Date of Birth:',
+                value: '26.04.2001',
+              },
+              {
+                label: 'Place of Residence',
+                value: 'Trabzon',
+              },
+              {
+                label: 'Education',
+                value: "Bachelor's Degree in Turkish Language and Literature, Karadeniz Technical University, 2024",
+              },
+              {
+                label: 'Preferred Role',
+                value: 'Frontend, UI',
+              },
+            ],
+
+            aboutMe: {
+                title: 'About Me',
+                description: "After graduating from Karadeniz Technical University with a degree in Turkish Language and Literature, I applied to the WorkinTech Fullstack Program and started my journey in software development.",
+              },
+            }, 
+         
+    projects: [
+        {
+          title: 'Workintech',
+          description:
+          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          icons: ['React', 'Redux', 'Vercel'],
+          
+        },
+        {
+          title: 'Journey',
+          description:
+            'A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.',
+          icons: ['React', 'Redux', 'Vercel'],
+          
+        },
+    ],
+    
+    footer: {
+        title: "Send me a message !",
+        description: "Got a question or proposal, or just want to say hello ? Go ahead."
+    }
+  },
+
+    tr : {
+
+      header: {
+        name: "Ceyda",
+    }, 
+    
+    hero: {
+        role: "Ben bir Frontend Geliştiriciyim.",
+        description:
+        "...Harika kullanıcı deneyimleriyle sağlam ve ölçeklenebilir frontend ürünleri oluşturmayı seven biri.",
     },
 
     skills: {
@@ -46,9 +119,7 @@ export const data= {
 
             aboutMe: {
                 title: 'Hakkımda',
-                description: `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.
-                Mnima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam! 
-                `,
+                description: "Karadeniz Teknik Üniversitesi'nden Türk Dili ve Edebiyatı bölümüyle mezun olduktan sonra, WorkinTech Fullstack Programı'na başvurdum ve yazılım geliştirme yolculuğuma başladım.",
               },
             }, 
          
@@ -56,22 +127,24 @@ export const data= {
         {
           title: 'Workintech',
           description:
-          "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          "Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmelerine olanak tanıyan basit, özelleştirilebilir ve minimal bir kurulum çerez eklentisi. Bu, vanilla JS, SCSS ve Parcel Bundler kullanılarak oluşturulmuş olup, NPM paketi olarak mevcuttur ve Git depolama alanı, kod ve temalar üzerinde her türlü özelleştirmeyi mümkün kılar.",
           icons: ['React', 'Redux', 'Vercel'],
           
         },
         {
-          title: 'Journey',
+          title: 'Yolculuk',
           description:
-            'A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.',
+            'Kullanıcılarınızın hangi çerezleri kabul edeceğini veya reddedeceğini seçmelerine olanak tanıyan basit, özelleştirilebilir ve minimal bir kurulum çerez eklentisi. Bu, vanilla JS, SCSS ve Parcel Bundler kullanılarak oluşturulmuş olup, NPM paketi olarak mevcuttur ve Git depolama alanı, kod ve temalar üzerinde her türlü özelleştirmeyi mümkün kılar.',
           icons: ['React', 'Redux', 'Vercel'],
           
         },
     ],
     
     footer: {
-        title: "Send me a message !",
-        description: "Got a question or proposal, or just want to say hello ? Go ahead."
+        title: "Bana mesaj gönder ! ",
+        description: "Bir sorunuz veya teklifiniz mi var, yoksa sadece merhaba demek mi istiyorsunuz? Buyurun."
     }
-    }
+
+  }
+}
 
